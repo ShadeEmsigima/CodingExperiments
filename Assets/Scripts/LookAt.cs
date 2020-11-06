d0c761846d774284c042f99816c95d5122f31706
Shade Desktop <gamingkendoka@gmail.com>
2020-11-06 16:04:11 +0100

    Created movementsystem, first prefabvariant
    :D

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This might be a nice script for when you talk to someone and they turn to see you (if theyre interested)
+public class LookAt : MonoBehaviour
+{
+    bool IsPiqued = true;
+    public GameObject Target = GameObject.FindGameObjectWithTag("Player");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        
+    }
+
+    //void WhenTheyStartTalking()
+    //            if (IsPiqued == true) {
+    //       //rotate NPC towards the player
+    //      transform.LookAt(Target.transform);
+    //    }
+
+}
---
+++ Assets/Scripts/MyPlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this will be the basic movement script for the character in ->MVP<- form
+
+public class MyPlayerController : MonoBehaviour
+{
+    [Tooltip("The speed at which this walks ya dummy")]
+    public float WalkSpeed = 5;
+
+
+    void Start()
+    {
+        Debug.Log("Game start");
+    }
+
+    
+    void Update()
+    {
+        // if pressing 'w' > go forwards by 'walk speed'
+        if (Input.GetKey("w")) {
+            transform.position += transform.forward * Time.deltaTime * WalkSpeed;
+        }
+
+        // if pressing 'a' > go 'left'
+        if (Input.GetKey("d")) { 
+            transform.position += transform.right * Time.deltaTime * WalkSpeed;
+        }
+
+        // if pressing 'd' > go 'right'
+        if (Input.GetKey("a")) {
+            transform.position += transform.right * -Time.deltaTime * WalkSpeed;
+        }
+
+        // if pressing 's' > go 'backwards'
+        if (Input.GetKey("s")) {
+            transform.position += transform.forward * -Time.deltaTime * WalkSpeed;
+        }
+    }
+}
